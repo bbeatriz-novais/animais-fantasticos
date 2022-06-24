ad00e1035af02fcae02f0b80dbc7ffb879c8d5fd
Yasmin <y.novaisouza@gmail.com>
2022-06-24 13:48:46 -0300

    bugs fixed

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -12,7 +12,8 @@ export default class MenuMobile {
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
+  openMenu(event) {
+    event.preventDefault();
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
@@ -26,7 +27,7 @@ export default class MenuMobile {
   }
 
   init() {
-    if (this.menuButton.length && this.menuList) {
+    if (this.menuButton && this.menuList) {
       this.addMenuMobileEvents();
     }
     return this;
